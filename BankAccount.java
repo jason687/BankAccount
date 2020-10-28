7051860717ddd39218e7f0a5c6ed57b63a20c731
Jason Zhang <jzhang20@stuy.edu>
2020-10-28 01:22:30 -0400

    deposit and withdraw

--- BankAccount.java
+++ BankAccount.java
@@ -17,7 +17,34 @@ public class BankAccount {
     return accountID;
   }
 
+  public String getPassword() {
+    return password;
+  }
+
   public String toString() {
     return accountID + "\t" + balance;
   }
+
+  public void setPassword (String newPass) {
+    password = newPass;
+  }
+
+  public boolean deposit (double amount) {
+    if (amount >= 0) {
+      balance = balance + amount;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public boolean withdraw (double amount) {
+    if (amount >= 0 && amount <= balance) {
+      balance = balance - amount;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
